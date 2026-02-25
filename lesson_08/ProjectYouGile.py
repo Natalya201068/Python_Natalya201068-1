e029abacc6e63cd6a5099acb391ad2ad52175e1a
Natalya201068 <dimedroll2010@gmail.com>
2026-02-25 16:16:10 +0300

    ninth commit

---
+++ lesson_08/ProjectYouGile.py
@@ -0,0 +1,74 @@
+import requests
+from dotenv import load_dotenv
+import os
+
+
+class ProjectYouGile:
+
+    def __init__(self, url) -> None:
+        self.url = url
+        load_dotenv()
+
+    def create_new_project(self, title, user_id):
+        url = f'{self.url}/projects'
+        key = os.getenv("KEY")
+        token = f'Bearer {key}'
+        headers = {
+            'Authorization': token,
+            'Content-Type': 'application/json'
+        }
+        data = {
+            'title': title,
+            'users': {
+                user_id: "admin"
+            }
+        }
+        response = requests.post(url, headers=headers, json=data)
+        status_code = response.status_code
+        project = response.json()
+        return status_code, project
+
+    def change_new_project(self, id_project, new_title, user_id):
+        url = f'{self.url}/projects/{id_project}'
+        key = os.getenv("KEY")
+        token = f'Bearer {key}'
+        headers = {
+            'Authorization': token,
+            'Content-Type': 'application/json'
+        }
+        data = {
+            'title': new_title,
+            'users': {
+                user_id: "admin"
+            }
+        }
+        response = requests.put(url, headers=headers, json=data)
+        status_code = response.status_code
+        new_project = response.json()
+        return status_code, new_project
+
+    def get_new_project(self, id_project):
+        url = f'{self.url}/projects/{id_project}'
+        key = os.getenv("KEY")
+        token = f'Bearer {key}'
+        headers = {
+            'Authorization': token,
+            'Content-Type': 'application/json'
+        }
+        response = requests.get(url, headers=headers)
+        status_code = response.status_code
+        project_id = response.json()
+        return status_code, project_id
+
+    def delete_project(self, id_project):
+        url = f'{self.url}/projects/{id_project}'
+        key = os.getenv("KEY")
+        token = f'Bearer {key}'
+        headers = {
+            'Authorization': token,
+            'Content-Type': 'application/json'
+        }
+        response = requests.delete(url, headers=headers)
+        status_code = response.status_code
+        r = response.json()
+        return status_code, r
---
+++ lesson_08/test_yougile.py
@@ -0,0 +1,67 @@
+import pytest
+from dotenv import load_dotenv
+import os
+from ProjectYouGile import ProjectYouGile
+
+
+load_dotenv()
+
+
+@pytest.fixture(scope="session")
+def api():
+    url = "https://ru.yougile.com/api-v2"
+    return ProjectYouGile(url)
+
+
+@pytest.fixture
+def temp_project(api):
+    title = 'Test Project'
+    user_id = os.getenv("USER_ID")
+    status, project = api.create_new_project(title, user_id)
+    assert status == 201, "Не удалось создать проект для теста"
+    yield project
+    api.delete_project(project["id"])
+
+
+def test_create_new_project(api):
+    title = 'Project2'
+    user_id = os.getenv("USER_ID")
+    status, project = api.create_new_project(title, user_id)
+    assert status == 201
+    assert project["id"] is not None
+    api.delete_project(project["id"])
+
+
+def test_create_new_project_negative(api):
+    title = 'Project2'
+    status, project = api.create_new_project(title, '12345')
+    assert status == 400
+    assert "Сотрудники со следующими ID не найдены в компании: 12345" in project.get("message")
+
+
+def test_change_new_project(api, temp_project):
+    new_title = 'Project3'
+    user_id = os.getenv("USER_ID")
+    status, new_project = api.change_new_project(temp_project['id'], new_title, user_id)
+    assert status == 200
+    assert new_project is not None
+
+
+def test_change_new_project_negative(api):
+    new_title = 'Project3'
+    user_id = os.getenv("USER_ID")
+    status, new_project = api.change_new_project('67890', new_title, user_id)
+    assert status == 404
+    assert "Проект не найден" in new_project.get("message")
+
+
+def test_get_new_project(api, temp_project):
+    status, project_data = api.get_new_project(temp_project['id'])
+    assert status == 200
+    assert project_data['id'] == temp_project['id']
+
+
+def test_get_new_project_negative(api):
+    status, project_id = api.get_new_project(api)
+    assert status == 404
+    assert 'Проект не найден' in project_id.get("message")
